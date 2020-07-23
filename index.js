524f7a7daf6bf3c1abdf494aa9b10cb40523b101
Marcus Pöhls <marcus.poehls@gmail.com>
2020-07-23 14:36:57 +0200

    export

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const HapiInCloudFunction = require('./src/index')
+const HapiInCloudFunction = require('./src')
 
 module.exports = HapiInCloudFunction
 module.exports.default = HapiInCloudFunction
